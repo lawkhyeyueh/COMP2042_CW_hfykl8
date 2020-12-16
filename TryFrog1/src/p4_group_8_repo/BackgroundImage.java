cc4603cf374ec1ec1c2d0d9ca4e6fa5a25451582
lawkhyeyueh <lawkhyeyueh@KhyeYueh-HPLaptop>
2020-12-16 12:55:18 +0800

    First Commit:
    Uploading All Files

---
+++ TryFrog1/src/p4_group_8_repo/BackgroundImage.java
@@ -0,0 +1,20 @@
+package p4_group_8_repo;
+
+import javafx.scene.image.Image;
+
+public class BackgroundImage extends Actor{
+
+	@Override
+	public void act(long now) {
+		
+		
+	}
+	
+	public BackgroundImage(String imageLink) {
+		setImage(new Image("file:src/p4_group_8_repo/iKogsKW.png", 600, 800, true, true));
+		
+	
+		
+	}
+
+}
